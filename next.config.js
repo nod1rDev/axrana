10edb0ad4a035ed1ecee96524e34003f4dd5632e
nod1rDev <nodirbeknegmatov2008@gmail.com>
2024-06-27 18:37:58 +0500

    update

--- next.config.js
+++ next.config.js
@@ -12,6 +12,8 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+ 
+};
 
 module.exports = withPWA(nextConfig);

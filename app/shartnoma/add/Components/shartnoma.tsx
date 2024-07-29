674de065926ab4f7bbb7c97ff94a57af0499f661
nod1rDev <nodirbeknegmatov2008@gmail.com>
2024-07-29 17:08:31 +0500

    done

--- app/shartnoma/add/Components/shartnoma.tsx
+++ app/shartnoma/add/Components/shartnoma.tsx
@@ -8,19 +8,96 @@ import FormControl from "@mui/material/FormControl";
 import SearchIcon from "@mui/icons-material/Search";
 import CreateShartnoma from "./CreateShartnoma";
 import { latinToCyrillic } from "@/app/tip/add/Components/lotin";
-import { InputAdornment, TextField } from "@mui/material";
-import { searchByClintName } from "@/app/Api/Apis";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { URL, searchByClintName } from "@/app/Api/Apis";
 import { useDispatch, useSelector } from "react-redux";
+import { styled } from "@mui/material/styles";
 import { alertChange } from "@/app/Redux/ShaxsiySlice";
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 function Shartnoma() {
   const [isLotin, setIsLotin] = useState("uz");
   const router = useRouter();
+  const [file, setFile] = useState<any>(null);
   const [value, setValue] = useState<any>({
     clientName: "",
   });
   const [data, setData] = useState<any>();
   const JWT = useSelector((s: any) => s.auth.JWT);
+  const handleFileChange = (event: any) => {
+    const selectedFile = event.target.files?.[0];
 
+    setFile(selectedFile);
+  };
+  const textToJson = (text: string) => {
+    try {
+      const jsonObject = JSON.parse(text);
+      return jsonObject;
+    } catch (error) {
+      return { success: false, message: "Invalid JSON format" };
+    }
+  };
+  const admin = useSelector((s: any) => s.auth.admin);
+  const dispatch = useDispatch();
+  const handleSubmit = async () => {
+    if (!file) return;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${JWT}`);
+
+    const formdata = new FormData();
+    formdata.append("file", file);
+
+    const requestOptions: any = {
+      method: "POST",
+      headers: myHeaders,
+      body: formdata,
+      redirect: "follow",
+    };
+
+    fetch(URL + "/contract/import/excel/data", requestOptions)
+      .then((response) => response.text())
+      .then((result: any) => {
+        const res = textToJson(result);
+        if (res.success) {
+          dispatch(
+            alertChange({
+              open: true,
+              message: latinToCyrillic("Exel file kiritildi"),
+              status: "success",
+            })
+          );
+
+          router.push("/shartnoma");
+        } else {
+          dispatch(
+            alertChange({
+              open: true,
+              message: latinToCyrillic(res.message),
+              status: "error",
+            })
+          );
+        }
+      })
+      .catch((error) =>
+        dispatch(
+          alertChange({
+            open: true,
+            message: error,
+            status: "error",
+          })
+        )
+      );
+  };
   return (
     <div className="w-[90%] mt-[5vh] mx-auto">
       <div className="flex justify-center mb-6 text-[28px] font-bold">
@@ -34,8 +111,38 @@ function Shartnoma() {
         >
           {latinToCyrillic("Orqaga")}
         </Button>
-
-        <div></div>
+      </div>
+      <div className="flex w-full justify-between">
+        <div className="flex gap-4 items-center">
+          <Button
+            component="label"
+            role={undefined}
+            variant="contained"
+            tabIndex={-1}
+            color="success"
+            startIcon={<CloudUploadIcon />}
+          >
+            {latinToCyrillic("Exel file yuklash")}
+            <VisuallyHiddenInput
+              type="file"
+              hidden
+             
+              onChange={handleFileChange}
+            />
+          </Button>
+          {file && (
+            <Button
+              onClick={handleSubmit}
+              variant="contained"
+              color="secondary"
+            >
+              {latinToCyrillic("Fileni Yuklash")}
+            </Button>
+          )}
+        </div>
+        <Button type="submit" variant="contained">
+          {latinToCyrillic("Qo'shish")}
+        </Button>
       </div>
       <CreateShartnoma />
     </div>

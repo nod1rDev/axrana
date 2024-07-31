de478986afb45ec78e387db27cdc6a31601403ee
nod1rDev <nodirbeknegmatov2008@gmail.com>
2024-08-01 02:06:55 +0500

    done

--- app/Components/MenuBar2.tsx
+++ app/Components/MenuBar2.tsx
@@ -1,29 +1,79 @@
 import * as React from "react";
+import { styled, alpha } from "@mui/material/styles";
 import Button from "@mui/material/Button";
-import { useRouter } from "next/navigation";
-import DonutSmallIcon from "@mui/icons-material/DonutSmall";
-import DnsIcon from "@mui/icons-material/Dns";
-import TokenIcon from "@mui/icons-material/Token";
-import StorageIcon from "@mui/icons-material/Storage";
+import Menu, { MenuProps } from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance"; // Icon for BXM
+import StorageIcon from "@mui/icons-material/Storage"; // Icon for Hisob raqami
 import { latinToCyrillic } from "../tip/add/Components/lotin";
+import { useRouter } from "next/navigation";
+import DnsIcon from "@mui/icons-material/Dns";
+import DonutSmallIcon from "@mui/icons-material/DonutSmall";
+import TokenIcon from "@mui/icons-material/Token";
+const StyledMenu = styled((props: MenuProps) => (
+  <Menu
+    elevation={0}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "left",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "left",
+    }}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiPaper-root": {
+    borderRadius: 6,
+    marginTop: theme.spacing(1),
+    minWidth: 260,
+    color:
+      theme.palette.mode === "light"
+        ? "rgb(55, 65, 81)"
+        : theme.palette.grey[300],
+    boxShadow:
+      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+    "& .MuiMenu-list": {
+      padding: "4px 0",
+    },
+    "& .MuiMenuItem-root": {
+      "& .MuiSvgIcon-root": {
+        fontSize: 24,
+        color: theme.palette.text.secondary,
+        marginRight: theme.spacing(1.5),
+      },
+      "&:active": {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          theme.palette.action.selectedOpacity
+        ),
+      },
+    },
+  },
+}));
 
 export default function MenuBar2() {
-  const [showMenu, setShowMenu] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   const router = useRouter();
-
-  const handleButtonClick = () => {
-    setShowMenu(!showMenu);
-  };
-
-  const handleMenuItemClick = (path: string) => {
-    router.push(path);
-    setShowMenu(false); // Hide the menu after navigation
-  };
 
   return (
     <div className="w-full">
       <Button
+        id="demo-customized-button"
+        aria-controls={open ? "demo-customized-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
         variant="contained"
+        disableElevation
         sx={{
           width: "100%",
           alignItems: "center",
@@ -33,50 +83,77 @@ export default function MenuBar2() {
           fontWeight: "bold",
           textAlign: "start",
         }}
-        onClick={handleButtonClick}
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon />}
       >
         {latinToCyrillic("Hisobot")}
       </Button>
-      {showMenu && (
-        <div className="mt-2">
-          <div
-            className="flex items-center cursor-pointer p-2"
-            onClick={() => handleMenuItemClick("/umumiy")}
-          >
+      <StyledMenu
+        id="demo-customized-menu"
+        MenuListProps={{
+          "aria-labelledby": "demo-customized-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <MenuItem
+          onClick={() => {
+            router.push("/umumiy");
+            handleClose();
+          }}
+          disableRipple
+        >
+          <div className="flex items-center  w-full">
             <DonutSmallIcon />
-            <span style={{ fontSize: "17px", fontWeight: "bold", marginLeft: "8px" }}>
+            <span style={{ fontSize: "17px", fontWeight: "bold" }}>
               Бригада умумий ҳисобот
             </span>
           </div>
-          <div
-            className="flex items-center cursor-pointer p-2"
-            onClick={() => handleMenuItemClick("/main")}
-          >
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            router.push("/main");
+            handleClose();
+          }}
+          disableRipple
+        >
+          <div className="flex items-center  w-full">
             <DnsIcon />
-            <span style={{ fontSize: "17px", fontWeight: "bold", marginLeft: "8px" }}>
+            <span style={{ fontSize: "17px", fontWeight: "bold" }}>
               батальон умумий ҳисобот
             </span>
           </div>
-          <div
-            className="flex items-center cursor-pointer p-2"
-            onClick={() => handleMenuItemClick("/maxsus")}
-          >
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            router.push("/maxsus");
+            handleClose();
+          }}
+          disableRipple
+        >
+          <div className="flex items-center  w-full">
             <TokenIcon />
-            <span style={{ fontSize: "17px", fontWeight: "bold", marginLeft: "8px" }}>
+            <span style={{ fontSize: "17px", fontWeight: "bold" }}>
               Бригада ҳисобот
             </span>
           </div>
-          <div
-            className="flex items-center cursor-pointer p-2"
-            onClick={() => handleMenuItemClick("/otchot")}
-          >
+        </MenuItem>
+        <MenuItem
+          onClick={() => {
+            router.push("/otchot");
+            handleClose();
+          }}
+          disableRipple
+        >
+          <div className="flex items-center  w-full">
             <StorageIcon />
-            <span style={{ fontSize: "17px", fontWeight: "bold", marginLeft: "8px" }}>
+            <span style={{ fontSize: "17px", fontWeight: "bold" }}>
               батальон ҳисобот
             </span>
           </div>
-        </div>
-      )}
+        </MenuItem>
+      </StyledMenu>
     </div>
   );
 }
